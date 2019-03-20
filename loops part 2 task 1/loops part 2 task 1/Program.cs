fce1381938356fc0000a76bf506006885097a654
Daniil Blair <30020332@student.toiohomai.ac.nz>
2019-03-21 12:55:59 +1300

    comment

---
+++ loops part 2 task 1/loops part 2 task 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops_part_2_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //My Name Is Dan And Im The Mastermind Behind This Program
+        {
+            Console.WriteLine("This Is The While Loop ");
+
+            int start = 76;
+            int finish = 46;
+            while (start >= finish)
+            {
+
+                if (start % 2 != 0)
+                    Console.WriteLine(start);
+                start--;
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
